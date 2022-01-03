e9d18294d6c4bcc7f8d4bca3a949825697749a80
dongkakika <takurway@gmail.com>
2022-01-04 00:19:55 +0900

    starter project upload

---
+++ src/main/java/dev/mvc/PtogApplication.java
@@ -0,0 +1,13 @@
+package dev.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtogApplication.class, args);
+	}
+
+}
